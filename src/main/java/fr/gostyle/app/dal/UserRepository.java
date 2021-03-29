5d97e7dd56343033984cf430028c2265a864b91b
nicolas.bern <nicolas.bern@epsi.fr>
2021-03-29 10:25:41 +0200

    Route email / mdp

--- src/main/java/fr/gostyle/app/dal/UserRepository.java
+++ src/main/java/fr/gostyle/app/dal/UserRepository.java
@@ -8,4 +8,5 @@ import java.util.List;
 public interface UserRepository extends CrudRepository<User, Long> {
 
     List<User> findByName (String name);
+    User findByEmailAndMdp (String email, String mdp);
 }
