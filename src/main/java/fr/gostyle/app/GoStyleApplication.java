55ea9afc9fda27c246c115a9912c8f15ec2effd3
nicolas.bern <nicolas.bern@epsi.fr>
2021-03-30 08:48:44 +0200

    Allow CORS

--- src/main/java/fr/gostyle/app/GoStyleApplication.java
+++ src/main/java/fr/gostyle/app/GoStyleApplication.java
@@ -2,9 +2,27 @@ package fr.gostyle.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@RestController
 public class GoStyleApplication {
+
+    @Bean
+    public WebMvcConfigurer configurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:8081/", "http://localhost:8080/")
+                        .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH")
+                        .allowedHeaders("Content-Type", "Access-Control-Allow-Origin", "Authorization", "X-Requested-With");
+            }
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GoStyleApplication.class, args);
